a113a10b3e87cf122fedc5dcad9996ff7ba2c09c
James Tang <jytang10@gmail.com>
2019-09-29 22:11:50 -0700

    Created addToDo method

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
 import { AppLoading } from 'expo';
 import ToDo from './ToDo';
+import uuidv1 from "uuid/v1";
 
 
 const {height, width} = Dimensions.get("window");
@@ -26,6 +27,32 @@ export default class App extends React.Component {
     })
   }
 
+  _addToDo = () => {
+    const { newToDo } = this.state;
+    if(newToDo !== "") {
+      this.setState(prevState => {
+        const ID = uuidv1();
+        const newToDoObject = {
+          [ID]: {
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createdAt: Date.now()
+          }
+        };
+        const newState = {
+          ...prevState,
+          newToDo: "",
+          toDos: {
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        }
+        return { ...newState };
+      });
+    }
+  }
+
   render(){
     const { newToDo, loadedToDos} = this.state;
     if(!loadedToDos){
@@ -44,6 +71,7 @@ export default class App extends React.Component {
             placeholderTextColor={"#999"}
             returnKeyType={"done"}
             autoCorrect={false}
+            onSubmitEditing={this._addToDo}
           >
           </TextInput>
           <ScrollView contentContainerStyle={styles.toDos}>
